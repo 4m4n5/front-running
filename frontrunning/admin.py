3bcdc0c5d444fd63c37c41fb1dbc3b310663d4d4
Aman Shrivastava <aman.srivastava999@gmail.com>
2017-08-29 14:28:52 +0530

    Admin register

--- frontrunning/admin.py
+++ frontrunning/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(AppleCustomerOrder)
+admin.site.register(AppleFirmOrder)
+admin.site.register(FacebookCustomerOrder)
+admin.site.register(FacebookFirmOrder)
+admin.site.register(WallmartCustomerOrder)
+admin.site.register(WalmartFirmOrder)
--- frontrunning/models.py
+++ frontrunning/models.py
@@ -1,64 +1,78 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 # Create your models here.
 class AppleCustomerOrder(models.Model):
 	trade_id = models.PositiveIntegerField(primary_key=True, null=False)
 	customer_id = models.PositiveIntegerField()
-	trade_type = models.models.CharField(max_length=200, null=False)
+	trade_type = models.CharField(max_length=200, null=False)
 	security_type = models.CharField(max_length=200, null=False)
 	security_name = models.CharField(max_length=200, null=False)
 	price = models.FloatField(null=False)
 	quantity = models.PositiveIntegerField(null=False)
 	datetime = models.DateTimeField(auto_now=True)
+	def __str__(self):
+		return self.trade_id
 
 
 class AppleFirmOrder(models.Model):
 	trade_id = models.PositiveIntegerField(primary_key=True, null=False)
 	customer_id = models.PositiveIntegerField()
-	trade_type = models.models.CharField(max_length=200, null=False)
+	trade_type = models.CharField(max_length=200, null=False)
 	security_type = models.CharField(max_length=200, null=False)
 	security_name = models.CharField(max_length=200, null=False)
 	price = models.FloatField(null=False)
 	quantity = models.PositiveIntegerField(null=False)
 	datetime = models.DateTimeField(auto_now=True)
+	def __str__(self):
+		return self.trade_id
 
 
 class FacebookCustomerOrder(models.Model):
 	trade_id = models.PositiveIntegerField(primary_key=True, null=False)
 	customer_id = models.PositiveIntegerField()
-	trade_type = models.models.CharField(max_length=200, null=False)
+	trade_type = models.CharField(max_length=200, null=False)
 	security_type = models.CharField(max_length=200, null=False)
 	security_name = models.CharField(max_length=200, null=False)
 	price = models.FloatField(null=False)
 	quantity = models.PositiveIntegerField(null=False)
 	datetime = models.DateTimeField(auto_now=True)
+	def __str__(self):
+		return self.trade_id
 
 class FacebookFirmOrder(models.Model):
 	trade_id = models.PositiveIntegerField(primary_key=True, null=False)
 	customer_id = models.PositiveIntegerField()
-	trade_type = models.models.CharField(max_length=200, null=False)
+	trade_type = models.CharField(max_length=200, null=False)
 	security_type = models.CharField(max_length=200, null=False)
 	security_name = models.CharField(max_length=200, null=False)
 	price = models.FloatField(null=False)
 	quantity = models.PositiveIntegerField(null=False)
 	datetime = models.DateTimeField(auto_now=True)
+	def __str__(self):
+		return self.trade_id
 
 class WallmartCustomerOrder(models.Model):
 	trade_id = models.PositiveIntegerField(primary_key=True, null=False)
 	customer_id = models.PositiveIntegerField()
-	trade_type = models.models.CharField(max_length=200, null=False)
+	trade_type = models.CharField(max_length=200, null=False)
 	security_type = models.CharField(max_length=200, null=False)
 	security_name = models.CharField(max_length=200, null=False)
 	price = models.FloatField(null=False)
 	quantity = models.PositiveIntegerField(null=False)
 	datetime = models.DateTimeField(auto_now=True)
+	def __str__(self):
+		return self.trade_id
 
 class WalmartFirmOrder(models.Model):
 	trade_id = models.PositiveIntegerField(primary_key=True, null=False)
 	customer_id = models.PositiveIntegerField()
-	trade_type = models.models.CharField(max_length=200, null=False)
+	trade_type = models.CharField(max_length=200, null=False)
 	security_type = models.CharField(max_length=200, null=False)
 	security_name = models.CharField(max_length=200, null=False)
 	price = models.FloatField(null=False)
 	quantity = models.PositiveIntegerField(null=False)
 	datetime = models.DateTimeField(auto_now=True)
+	def __str__(self):
+		return self.trade_id
