ed353a10a364c5b171d1c1a1dff1724b2f022030
Aman Shrivastava <aman.srivastava999@gmail.com>
2017-08-29 14:48:56 +0530

    Serve Template

--- frontrunning/views.py
+++ frontrunning/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+from frontrunning.models import *
+
+import json
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	return render_to_response('index.html')
--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(os.path.dirname(__file__) , '../templates').replace('\\','/')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
